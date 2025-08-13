8b316a87002f16f07775220c33bfc9ef97df35dc
jay.choi <jay.choi@kakaocorp.com>
2025-08-13 10:29:50 +0900

    updapte package version

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ import PackageDescription
 let package = Package(
     name: "SwiftYFinance",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16),
-        .tvOS(.v16),
-        .watchOS(.v9)
+        .macOS(.v15),
+        .iOS(.v18),
+        .tvOS(.v18),
+        .watchOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
